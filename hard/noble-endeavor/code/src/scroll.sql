2df28b122b6ca2944a782a7b628e15c126cc36ac
Akalanka Perera <akalankaperera128@gmail.com>
2024-07-10 17:11:49 +0530

    Feat: added noble endeavor

--- hard/noble-endeavor/code/src/scroll.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE xc90;
-    USE xc90;
-    CREATE TABLE est (comment, name, password);
-    INSERT INTO est VALUES (78, 69, 67), (22, 20, 42), (26, 36, 52), (84, 58, 13), (10, 69, 99), (26, 61, 6), (20, 18, 15), (86, 18, 19), (98, 17, 66), (46, 31, 99);
-    
--- hard/noble-endeavor/code/test/index.test.js
+++ hard/noble-endeavor/code/test/index.test.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
+const mongodb = require("mongodb")
 const exec = require('@sliit-foss/actions-exec-wrapper').default;
 const { faker } = require('@faker-js/faker');
 
-jest.setTimeout(25000);
+jest.setTimeout(300000);
 
 test('should migrate the data from the sql file to a mongodb database', async () => {
     if (fs.existsSync('./src')) {
@@ -12,21 +13,31 @@ test('should migrate the data from the sql file to a mongodb database', async ()
     const database = faker.vehicle.model()?.split(' ')?.[0]?.toLowerCase();
     const table = faker.lorem.word();
     const columns = new Set();
-    for (let i = 0; i < faker.datatype.number({ min: 3, max: 15 }); i++) {
-        columns.add(faker.database.column());
+    for (let i = 0; i < faker.number.int({ min: 10, max: 25 }); i++) {
+        const c = faker.database.column();
+        if (c != "id") columns.add(c);
     }
-    const rows = faker.datatype.number({ min: 10, max: 10 });
+    const rows = faker.number.int({ min: 10, max: 10 });
     const data = Array.from({ length: rows }, () => {
         return Array.from(columns).reduce((acc, column) => {
-            acc[column] = faker.datatype.number({ min: 0, max: 100 });
+            acc[column] = faker.number.int({ min: 0, max: 100 });
             return acc;
         }, {});
     });
     const sql = `CREATE DATABASE ${database};
     USE ${database};
-    CREATE TABLE ${table} (${Array.from(columns).join(', ')});
-    INSERT INTO ${table} VALUES ${data.map(row => `(${Object.values(row).join(', ')})`).join(', ')};
-    `;
-    fs.writeFileSync('./src/scroll.sql', sql);
+    CREATE TABLE ${table} (${Array.from(columns).map(column => `\`${column}\` INT`).join(', ')});
+    INSERT INTO ${table} VALUES ${data.map(row => `(${Object.values(row).join(', ')})`).join(', ')};`;
+    const scrollName = faker.lorem.word();
+    fs.writeFileSync(`./src/scroll-${scrollName}.sql`, sql);
     await exec('bash execute.sh');
+    const client = new mongodb.MongoClient(`mongodb://admin:3aDAl3vfeRbY1lf@localhost:27020/${database}?directConnection=true`);
+    await client.connect();
+    const db = client.db(database);
+    const insertedRows = await db.collection(table).find().toArray();
+    expect(insertedRows).toHaveLength(rows);
+    insertedRows.forEach((row, i) => {
+        expect(row).toMatchObject(data[i]);
+    });
+    await client.close();
 });
