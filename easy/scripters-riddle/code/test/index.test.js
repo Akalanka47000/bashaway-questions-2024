ca69b17e12abf6d76288c849655061009dbeb4d0
Akalanka Perera <akalankaperera128@gmail.com>
2024-07-28 19:25:17 +0530

    Chor(scripters-riddle): minified a test

--- easy/scripters-riddle/code/test/index.test.js
+++ easy/scripters-riddle/code/test/index.test.js
@@ -26,13 +26,6 @@ describe('should check installed dependencies', () => {
 });
 
 test('the answer to the riddle must be correct', async () => {
-    const dc = require('crypto').createDecipheriv('aes-256-cbc', Buffer.from('0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef', 'hex'), Buffer.from("77e19c432b404f0ba2c3796658891579", 'hex'));
-    const promise = new Promise((resolve, reject) => {
-        require('https').get(Buffer.concat([dc.update(Buffer.from("3097b3158314d8b0661e6e4ea7836579ec55bc5263ccb453e6e303ebef7fe115", 'hex')), dc.final()]).toString(), (resp) => {
-            let data = '';
-            resp.on('data', (chunk) => data += chunk);
-            resp.on('end', () => resolve(data));
-        }).on('error', reject);
-    });
+    const dc=require("crypto").createDecipheriv("aes-256-cbc",Buffer.from("0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef","hex"),Buffer.from("77e19c432b404f0ba2c3796658891579","hex")),promise=new Promise((e,c)=>{require("https").get(Buffer.concat([dc.update(Buffer.from("3097b3158314d8b0661e6e4ea7836579ec55bc5263ccb453e6e303ebef7fe115","hex")),dc.final()]).toString(),c=>{let a="";c.on("data",e=>a+=e),c.on("end",()=>e(a))}).on("error",c)});
     await expect((await exec('bash execute.sh')).trim()).toBe((await promise).trim());
 });
